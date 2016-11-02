28d3ffaa0646f811c9ae25d811e1cb95afe852c2
numbqq <hbjsxq@163.com>
2016-11-02 19:21:05 +0800

    support DM9000.

--- linux-3.0.80/arch/arm/mach-s5pv210/mach-tiny210.c
+++ linux-3.0.80/arch/arm/mach-s5pv210/mach-tiny210.c
@@ -115,20 +115,25 @@ static struct samsung_keypad_platdata tiny210_keypad_data __initdata = {
 	.cols		= 8,
 };
 
+/*add by Nick.*/
+/*     physical address for dm9000a ...*/  
+#define S5PV210_PA_DM9000_A       (0x88000000)  
+#define S5PV210_PA_DM9000_F       (S5PV210_PA_DM9000_A + 0x300c) 
+
 static struct resource tiny210_dm9000_resources[] = {
 	[0] = {
-		.start	= S5PV210_PA_SROM_BANK5,
-		.end	= S5PV210_PA_SROM_BANK5,
+		.start	= S5PV210_PA_DM9000_A,
+		.end	= S5PV210_PA_DM9000_A + SZ_1K * 4 - 1,
 		.flags	= IORESOURCE_MEM,
 	},
 	[1] = {
-		.start	= S5PV210_PA_SROM_BANK5 + 2,
-		.end	= S5PV210_PA_SROM_BANK5 + 2,
+		.start	= S5PV210_PA_DM9000_F,
+		.end	= S5PV210_PA_DM9000_F + SZ_1K * 4 - 1,
 		.flags	= IORESOURCE_MEM,
 	},
 	[2] = {
-		.start	= IRQ_EINT(9),
-		.end	= IRQ_EINT(9),
+		.start	= IRQ_EINT(7),
+		.end	= IRQ_EINT(7),
 		.flags	= IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL,
 	},
 };
@@ -314,19 +319,19 @@ static struct platform_device *tiny210_devices[] __initdata = {
 
 static void __init tiny210_dm9000_init(void)
 {
-	unsigned int tmp;
+	unsigned int tmp;  
+	  
+	gpio_request(S5PV210_MP01(1), "nCS1");	
+	s3c_gpio_cfgpin(S5PV210_MP01(1), S3C_GPIO_SFN(2));	
+	gpio_free(S5PV210_MP01(1));  
 
-	gpio_request(S5PV210_MP01(5), "nCS5");
-	s3c_gpio_cfgpin(S5PV210_MP01(5), S3C_GPIO_SFN(2));
-	gpio_free(S5PV210_MP01(5));
+	tmp = ((0 << 28) | (0 << 24) | (5 << 16) | (0 << 12) | (0 << 8) | (0 << 4) | (0 << 0));  
+	__raw_writel(tmp, (S5P_SROM_BW + 0x08));	
 
-	tmp = (5 << S5P_SROM_BCX__TACC__SHIFT);
-	__raw_writel(tmp, S5P_SROM_BC5);
-
-	tmp = __raw_readl(S5P_SROM_BW);
-	tmp &= (S5P_SROM_BW__CS_MASK << S5P_SROM_BW__NCS5__SHIFT);
-	tmp |= (1 << S5P_SROM_BW__NCS5__SHIFT);
-	__raw_writel(tmp, S5P_SROM_BW);
+	tmp = __raw_readl(S5P_SROM_BW);  
+	tmp &= ~(0xf << 4);  
+	tmp |= (1 << 4);  
+	__raw_writel(tmp, S5P_SROM_BW);  
 }
 
 static struct i2c_board_info tiny210_i2c_devs0[] __initdata = {
